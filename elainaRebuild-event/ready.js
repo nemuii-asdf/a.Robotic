1b96999bbce3cd2beec629f7f589dbb79e08a19a
kiyozin_0 <84609184+nemuii-asdf@users.noreply.github.com>
2025-03-03 15:24:00 +0700

    Delete elainaRebuild-event directory

--- elainaRebuild-event/ready.js
+++
@@ -1,9 +0,0 @@
-const log = require('../elainaRebuild-utils/log.js')
-
-module.exports = {
-    name: 'ready',
-    execute(client, args) {
-        //console.log(args)
-        log.toConsole("Bot is ready")
-    }
-};
