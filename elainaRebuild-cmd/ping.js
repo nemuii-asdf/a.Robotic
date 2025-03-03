706a1d1fefcf7459cf87e29aff4f5843f46c870b
kiyozin_0 <84609184+nemuii-asdf@users.noreply.github.com>
2025-03-03 15:23:32 +0700

    Delete elainaRebuild-cmd directory

--- elainaRebuild-cmd/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports.run = (client, message, args) => {
-    message.channel.send("pong!")
-}
-
-module.exports.name = 'ping'
-module.exports.isEnable = true
--- elainaRebuild-cmd/say.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = (client, message, args) => {
-    const sayMessage = args.join(" ");
-	message.channel.send(sayMessage);
-}
-
-module.exports.name = 'say'
-module.exports.isEnable = true
